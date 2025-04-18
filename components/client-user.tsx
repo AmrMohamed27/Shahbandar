41b08bfa957edbdd55645f224096c23ea7802d22
AmrMohamed27 <amrmohamedorigi44@gmail.com>
2025-04-18 06:52:34 +0200

    chore: remove console logs

--- components/client-user.tsx
+++ components/client-user.tsx
@@ -4,7 +4,6 @@ import { authClient } from "@/lib/auth-client";
 
 const ClientUser = () => {
   const { data: session } = authClient.useSession();
-  console.log("Session data:", session);
   return <div>Hey, {session?.user.name}</div>;
 };
 
